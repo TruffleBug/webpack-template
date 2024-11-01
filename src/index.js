75b2af05d28b7fba29b2e226f975726769b7d439
TruffleBug <lisa.ho1022@gmail.com>
2024-11-01 13:56:51 -0600

    template

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
+
+//to add a local image to js file
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
