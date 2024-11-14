35a6a6ac5f5540fa5636847c293a437f8ebaefdf
TruffleBug <lisa.ho1022@gmail.com>
2024-11-14 12:07:42 -0700

    Added configs for prettier & ESLint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import "./styles.css";
-import { greeting } from "./greeting.js";
+import './styles.css'
+import { greeting } from './greeting.js'
 
-console.log(greeting);
+console.log(greeting)
 
 //to add a local image to js file
-import odinImage from "./odin.png";
-   
-const image = document.createElement("img");
-image.src = odinImage;
-   
-document.body.appendChild(image);
+import odinImage from './odin.png'
+
+const image = document.createElement('img')
+image.src = odinImage
+
+document.body.appendChild(image)
